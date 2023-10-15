412f8934e56d533d4bf200035228b08bc54663d9
Denis Costello <G00396309@atu.ie>
2023-10-15 13:55:22 +0100

    set notification to port 8083

--- src/main/java/ie/atu/notificationservice/NotificationController.java
+++ src/main/java/ie/atu/notificationservice/NotificationController.java
@@ -1,4 +1,13 @@
 package ie.atu.notificationservice;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class NotificationController {
+    @PostMapping("/notification")
+    public String sendNotification(@RequestBody UserDetails userDetails){
+        String notification = "User "
+    }
 }
